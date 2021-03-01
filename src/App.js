7302ffa9e723d8f3c69a7c020a055bacfa747e8a
Kim Nguyen <misskim451@gmail.com>
2021-02-28 19:31:42 -0600

    Finished navigation bar base. Will update front end a bit more and find out how to reuse pages.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,48 @@
+//default things
 import logo from "./logo.svg";
 import "./App.css";
 
+//routing libraries
 import React, { Fragment } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Route, Switch } from "react-router-dom";
 
-import About from './about';
-import Contact from './contact';
-import Home from './home';
+//routing
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Home from "./pages/home";
+import BookRec from "./pages/book-rec-hist";
+import Journal from "./pages/journal-hist";
+
+//navbar libraries
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
+
+{/* navigation bar components */}
+      <Navbar bg="dark" variant="dark" fixed="top">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Book Rec! </Nav.Link>
+          <Nav.Link href="/about">About </Nav.Link>
+          <Nav.Link href="/journal-hist">Journal History </Nav.Link>
+          <Nav.Link href="/book-rec-hist">Book Rec History </Nav.Link>
+        </Nav>
+      </Navbar>
+
+{/* content */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+{/* routing links to appropriate components */}
         <Router>
           <main>
-            <nav>
-              <ul>
-                <li>
-                  <a href="/home">Home</a>
-                </li>
-                <li>
-                  <a href="/about">About</a>
-                </li>
-                <li>
-                  <a href="/contact">Contact</a>
-                </li>
-              </ul>
-            </nav>
-
-            <Route path="/home" exact component={Home} />
+            <Route path="/" exact component={Home} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
+            <Route path="/book-rec-hist" component={BookRec} />
+            <Route path="/journal-hist" component={Journal} />
           </main>
         </Router>
       </header>
